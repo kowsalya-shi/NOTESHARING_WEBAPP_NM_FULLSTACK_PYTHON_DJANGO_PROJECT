b4329d42262b4dd9e196750e5b04a9b30e1e8786
kowsalya-shi <146564618+kowsalya-shi@users.noreply.github.com>
2024-04-09 12:21:37 +0530

    Delete Source code - Notes Sharing Web Application with Django Framework directory

--- Source code - Notes Sharing Web Application with Django Framework/UploadFiles/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class MyFileForm(forms.Form):
-    file_name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    file=forms.FileField(widget=forms.FileInput(attrs={'class':'form-control'}))
--- Source code - Notes Sharing Web Application with Django Framework/UploadFiles/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class MyFileUpload(models.Model):
-    file_name=models.CharField(max_length=50)
-    my_file=models.FileField()
-
-# Create your models here.
